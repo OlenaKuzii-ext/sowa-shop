a287eb7fe905fede3642473fbdf691886dabc5c4
olen-ka <okuziy7@gmail.com>
2019-09-17 21:17:12 +0000

    last

--- js/index.js
+++ js/index.js
@@ -1,19 +1,10 @@
 const cart = new Cart($('#cartModal'));
-const productList = new ProductList(
-  'products.json',
-  $('.products-container'),
-  cart
-);
+const productList = new ProductList('products.json',$('.products-container'),cart);
 
+document.querySelector('#find').addEventListener('click', searchWindow);
 
+function searchWindow() {
+    let search = document.querySelector('#search').value;
+    window.find(search, true, true, true, false, false, true);
 
-
-
-
-
-
-
-
-
-
-
+}
